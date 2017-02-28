3e842daad81c765b9e24026b85a9dba4abbfa572
Bhavyansh Mishra <Bhavyansh Mishra>
2017-02-28 13:34:45 -0600

    Add database integration for the app

--- Music/src/Player.java
+++ Music/src/Player.java
@@ -6,6 +6,8 @@ public class Player {
 	private Boolean playing;
 	private Boolean paused;
 	private Playlist currentPlaylist;
+
+	private ArrayList<Song> allSongs;
 	private ArrayList<User> allUsers;
 	private DatabaseConnector dbconn;
 	
@@ -13,24 +15,37 @@ public class Player {
 		this.currentUser = new User("Administrator", "root", "root");
 		this.allUsers = new ArrayList<User>();
 		this.allUsers.add(this.currentUser);
-		this.currentUser.getAllSongs().add(
-				new Song("Don't Wanna Know", 
-						240, 
-						"Maroon 5", 
-						"C:\\DataBM\\Songs\\DWK.mp3", 
-						"Maroon 5", 
-						30.00, 
-						"C:\\DataBM\\Icons\\DWK.png"));
-		this.currentUser.getAllSongs().add(new Song("The Best Day of My Life"));
-		this.currentUser.getAllSongs().add(new Song("Demons"));
-		this.currentUser.getAllSongs().add(new Song("Apologize"));
-		this.currentUser.getAllSongs().add(new Song("Numb"));
-		this.currentUser.getAllSongs().add(new Song("In The End"));
-		this.currentUser.getAllSongs().add(new Song("Stitches"));
-		this.currentUser.getAllSongs().add(new Song("Sugar"));
-		this.currentUser.getAllSongs().add(new Song("Radioactive"));
+//		this.currentUser.getAllSongs().add(
+//				new Song("Don't Wanna Know", 
+//						240, 
+//						"Maroon 5", 
+//						"C:\\DataBM\\Songs\\DWK.mp3", 
+//						"Maroon 5", 
+//						30.00, 
+//						"C:\\DataBM\\Icons\\DWK.png"));
+//		this.currentUser.getAllSongs().add(new Song("The Best Day of My Life"));
+//		this.currentUser.getAllSongs().add(new Song("Demons"));
+//		this.currentUser.getAllSongs().add(new Song("Apologize"));
+//		this.currentUser.getAllSongs().add(new Song("Numb"));
+//		this.currentUser.getAllSongs().add(new Song("In The End"));
+//		this.currentUser.getAllSongs().add(new Song("Stitches"));
+//		this.currentUser.getAllSongs().add(new Song("Sugar"));
+//		this.currentUser.getAllSongs().add(new Song("Radioactive"));
 		
 		this.dbconn = new DatabaseConnector();
+		this.setAllSongs(this.dbconn.getDbSongs());
+		dbconn.printSongs(this.allSongs);
+		for(Song s: this.allSongs){
+			this.currentUser.getAllSongs().add(s);
+		}
+	}
+	
+	public ArrayList<Song> getAllSongs() {
+		return allSongs;
+	}
+
+	public void setAllSongs(ArrayList<Song> allSongs) {
+		this.allSongs = allSongs;
 	}
 	
 	public void addUser(User user){

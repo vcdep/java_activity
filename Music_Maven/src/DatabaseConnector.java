74801b86a3d08405562c4f731a264e648b808141
Bhavyansh Mishra <Bhavyansh Mishra>
2017-03-08 00:14:01 -0600

    New changes to Music_Maven

--- Music_Maven/src/DatabaseConnector.java
+++ Music_Maven/src/DatabaseConnector.java
@@ -1,37 +1,26 @@
 import java.sql.*;
-import java.util.ArrayList;
 
 public class DatabaseConnector {
 	private Connection conn;
 	private Statement statement;	
 	private ResultSet rows;
 	private String query;
-	private ArrayList<Song> dbSongs;
-	private ArrayList<User> dbUsers;
 	
 	public DatabaseConnector(){
-		dbSongs = new ArrayList<Song>();
 		this.setQuery("select * from songs");
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			this.conn = DriverManager.getConnection("jdbc:mysql://52.90.84.220:8080/Tester", "naman", "chocolate");
+			this.conn = DriverManager.getConnection("jdbc:mysql://54.159.166.105:8080/Tester", "naman", "chocolate");
 			this.statement = this.conn.createStatement();
 			this.rows = this.statement.executeQuery(this.query);
 			while(this.rows.next()){
-				dbSongs.add(new Song(this.rows.getString("Name"), 
-						this.rows.getInt("Duration"), 
-						this.rows.getString("Album"), 
-						this.rows.getString("Location"), 
-						this.rows.getString("Singer"), 
-						this.rows.getDouble("Cost"), 
-						this.rows.getString("Icon")));
-//				System.out.println(this.rows.getInt("Srno")+"\t"+
-//									this.rows.getString("Name")+"\t"+
-//									this.rows.getInt("Duration")+"\t"+
-//									this.rows.getString("Singer")+"\t"+
-//									this.rows.getDouble("Cost")+"\t"+
-//									this.rows.getString("Location")+"\t"+
-//									this.rows.getString("Icon")+"\t");				
+				System.out.println(this.rows.getInt("Srno")+"\t"+
+									this.rows.getString("Name")+"\t"+
+									this.rows.getInt("Duration")+"\t"+
+									this.rows.getString("Singer")+"\t"+
+									this.rows.getDouble("Cost")+"\t"+
+									this.rows.getString("Location")+"\t"+
+									this.rows.getString("Icon")+"\t");
 			}
 		} catch (SQLException ex) {
 			System.out.println("SQLException: " + ex.getMessage());
@@ -39,17 +28,6 @@ public class DatabaseConnector {
 		} catch(ClassNotFoundException e){
 			e.printStackTrace();
 		}				
-	}
-	
-	public void printSongs(ArrayList<Song> songlist){
-		for(Song s: songlist){
-			System.out.println(s.getName() + "\t" + 
-					s.getDuration() + "\t" + 
-					s.getSinger() + "\t" + 
-					s.getCost() + "\t" + 
-					s.getLocation() + "\t" + 
-					s.getIcon() + "\t");
-		}
 	}
 	
 	public Connection getConn() {
@@ -75,22 +53,5 @@ public class DatabaseConnector {
 	}
 	public void setQuery(String query) {
 		this.query = query;
-	}
-
-	public ArrayList<Song> getDbSongs() {
-		return dbSongs;
-	}
-
-	public void setDbSongs(ArrayList<Song> dbSongs) {
-		this.dbSongs = dbSongs;
-	}
-
-	public ArrayList<User> getDbUsers() {
-		return dbUsers;
-	}
-
-	public void setDbUsers(ArrayList<User> dbUsers) {
-		this.dbUsers = dbUsers;
 	}	
-	
 }

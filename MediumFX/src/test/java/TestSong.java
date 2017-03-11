ddf1b9020bf36b7f1d6d3fa2c322ed8a64ddf0a6
Bhavyansh Mishra <Bhavyansh Mishra>
2017-03-11 16:47:38 -0600

    Add the right folder structure

--- MediumFX/src/test/java/TestSong.java
+++ MediumFX/src/test/java/TestSong.java
@@ -1,4 +1,4 @@
-package test;
+package test.java;
 import application.*;
 
 import static org.junit.Assert.*;
--- MediumFX/src/test/java/TestUser.java
+++ MediumFX/src/test/java/TestUser.java
@@ -1,4 +1,4 @@
-package test;
+package test.java;
 import application.User;
 
 import static org.junit.Assert.*;

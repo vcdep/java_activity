ee369da3eb9b41b90deed154abbb31541f0337c3
BhavyanshM <bhavyanshmishra@gmail.com>
2017-03-26 19:31:51 -0500

    Add correct resource paths

---
+++ MediumFX_v1/src/application/Controller.java
@@ -0,0 +1,234 @@
+package application;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Controller implements Initializable{
+	@FXML
+	private Button switchUser;
+	@FXML
+	private Label userLabel;
+	@FXML
+	private ListView<String> list;
+	@FXML
+	private Button searchButton;
+	@FXML
+	private Slider hslider;
+	@FXML
+	private Slider vslider;
+	@FXML
+	private Label bottomLabel;
+	@FXML
+	private Label currentTimeLabel;
+	@FXML
+	private Label durationLabel;
+	@FXML
+	private Label nameLabel;
+	@FXML
+	private Label albumLabel;
+	@FXML
+	private Label singerLabel;
+	@FXML
+	private ImageView playButton;
+	
+	private String tempTime;
+	private boolean stopRequested;
+	private boolean atEndOfMedia;
+	private Duration duration;
+	private Image image;
+	private ObservableList<String> items;
+	private Player player;
+	private FXMLLoader loader;
+	
+	public Controller(){
+		super();
+		
+		System.out.println("Controller created.");
+	}
+	
+	public void setList(ArrayList<Song> songs){
+		items = FXCollections.observableArrayList();
+		for(int i = 0; i<songs.size(); i++)
+			items.add(songs.get(i).getName());
+		this.list.setItems(items);
+	}
+	
+
+	@FXML
+	public void onSwitchUserClicked(ActionEvent event) throws Exception{
+		this.loader = new FXMLLoader();
+		Pane root = loader.load(getClass().getResource("/resources/login.fxml").openStream());
+		LoginController loginCont = (LoginController) loader.getController();
+		loginCont.setPlayer(player);
+		Stage window = (Stage) ((Control)event.getSource()).getScene().getWindow();
+		Scene scene = new Scene(root, 800, 600);		
+		window.setScene(scene);
+		window.show();
+	}
+	@FXML
+	public void onSearchButtonClicked(ActionEvent event) throws Exception{
+//		this.setList(player.getCurrentUser().getAllSongs());
+		System.out.println("Searching...");
+
+	}
+	@FXML
+	public void onIconOneClicked(MouseEvent mevent) throws Exception{
+		this.player.setMedia(this.player.media);
+		System.out.println("Song Selected...");
+		player.mplayer.currentTimeProperty().addListener(new InvalidationListener() 
+	      {
+	          public void invalidated(Observable ov) {
+	              updateValues();
+	          }
+	      });
+		
+		hslider.valueProperty().addListener(new InvalidationListener() {
+		    public void invalidated(Observable ov) {
+			       if (hslider.isValueChanging()) {
+			    	   
+			           player.mplayer.seek(duration.multiply(hslider.getValue() / 100.0));
+			       }
+			    }
+			});
+//
+//      player.mplayer.setOnPlaying(new Runnable() {           
+//
+//			public void run() {
+//				System.out.println("Running");
+//			 if (stopRequested) {
+//				 image = new Image("resources/pause.png");
+//				 playButton.setImage(image);
+//				 player.mplayer.pause();
+//				 stopRequested = false;
+//				 System.out.println("Playing...");
+//              } else {
+//                image = new Image("resources/play.png");
+//      			playButton.setImage(image);
+//      			player.setPlaying(false);
+//      			player.mplayer.play();
+//      			stopRequested = true;
+//      			System.out.println("Paused");
+//              }
+//          }
+//      });
+//
+//      player.mplayer.setOnPaused(new Runnable() {
+//          public void run() {
+//          	image = new Image("resources/play.png");
+//  			playButton.setImage(image);
+//  			player.setPlaying(false);
+//          }
+//      });
+//
+      player.mplayer.setOnReady(new Runnable() {
+          public void run() {
+              duration = player.mplayer.getMedia().getDuration();
+              updateValues();
+          }
+      });
+
+      player.mplayer.setOnEndOfMedia(new Runnable() {
+          public void run() {
+          	image = new Image("resources/play.png");
+  			playButton.setImage(image);
+              stopRequested = true;
+              player.mplayer.seek(player.mplayer.getStartTime());
+              player.mplayer.pause();
+              player.setPlaying(false);
+          }
+     });
+	}
+	
+	@FXML
+	public void onPlayButtonClicked(MouseEvent mevent) throws Exception{
+
+		if(!player.getPlaying()){
+			image = new Image("resources/pause.png");
+			this.playButton.setImage(image);
+			this.player.mplayer.play();;
+			this.player.setPlaying(true);
+			System.out.println("Playing...");
+			stopRequested = false;
+		} else{
+		
+			this.player.mplayer.pause();;
+			this.image = new Image("resources/play.png");
+			this.playButton.setImage(this.image);
+			this.player.setPlaying(false);
+			System.out.println("Paused");
+			stopRequested = true;
+		}
+		
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void setUserLabelText(String userName){
+		this.userLabel.setText(userName);
+	}
+	public void setPlayer(Player player){
+		this.player = player;
+		stopRequested = false;
+		vslider.valueProperty().addListener(new InvalidationListener() {
+		    public void invalidated(Observable ov) {
+			       if (vslider.isValueChanging()) {
+			           player.mplayer.setVolume(vslider.getValue() / 100.0);
+			       }
+			    }
+			});
+		
+		this.setList(player.getCurrentUser().getAllSongs());
+		
+
+	}
+	
+	protected void updateValues() {
+		  if (currentTimeLabel != null && hslider != null && vslider != null) {
+		     Platform.runLater(new Runnable() {
+		        public void run() {
+		        	Duration currentTime = player.mplayer.getCurrentTime();
+		        	tempTime = String.valueOf(currentTime);
+		        	tempTime = tempTime.substring(0,tempTime.length()-3);
+		        	int time = ((int)Double.parseDouble(tempTime))/1000; 
+		        	tempTime = String.format("%02d", time/60)+":"+String.format("%02d", time%60);
+		        	currentTimeLabel.setText(tempTime);
+
+		        	hslider.setDisable(duration.isUnknown());
+		        	if (!hslider.isDisabled() 
+		        			&& duration.greaterThan(Duration.ZERO) 
+		        			&& !hslider.isValueChanging()) {
+		        				hslider.setValue(currentTime.divide(duration).toMillis()
+		        						* 100.0);
+		        	}
+		        	if (!vslider.isValueChanging()) {
+		        		vslider.setValue((int)Math.round(player.mplayer.getVolume() 
+		        				* 100));
+		        	}
+		        }
+		     });
+		  }
+		}
+	
+}
---
+++ MediumFX_v1/src/application/Player.java
@@ -0,0 +1,104 @@
+package application;
+import java.util.ArrayList;
+import javafx.scene.media.*;
+
+public class Player {
+	private User currentUser;
+	private Song currentSong;
+	private Boolean playing;
+	private Playlist currentPlaylist;
+	private ArrayList<Song> allSongs;
+	private ArrayList<User> allUsers;
+	private DatabaseConnector dbconn;
+	public MediaPlayer mplayer;
+	public Media media;
+	
+	public Player(){
+		this.setPlaying(false); 
+		this.currentUser = new User("Administrator", "root", "root");
+		this.allUsers = new ArrayList<User>();
+		this.allUsers.add(this.currentUser);
+//		this.currentUser.getAllSongs().add(
+//				new Song("Don't Wanna Know", 
+//						240, 
+//						"Maroon 5", 
+//						"C:\\DataBM\\Songs\\DWK.mp3", 
+//						"Maroon 5", 
+//						30.00, 
+//						"C:\\DataBM\\Icons\\DWK.png"));
+//		this.currentUser.getAllSongs().add(new Song("The Best Day of My Life"));
+//		this.currentUser.getAllSongs().add(new Song("Demons"));
+//		this.currentUser.getAllSongs().add(new Song("Apologize"));
+//		this.currentUser.getAllSongs().add(new Song("Numb"));
+//		this.currentUser.getAllSongs().add(new Song("In The End"));
+//		this.currentUser.getAllSongs().add(new Song("Stitches"));
+//		this.currentUser.getAllSongs().add(new Song("Sugar"));
+//		this.currentUser.getAllSongs().add(new Song("Radioactive"));
+		
+		
+	}
+	
+	public void init(){
+		this.dbconnect();
+		this.currentUser.getAllSongs().get(0).setFile("src/resources/Dont_Wanna_Know.mp3");
+		this.media = new Media(currentUser.getAllSongs().get(0).getFile().toURI().toString());
+		
+	}
+	
+	public void dbconnect(){
+		this.dbconn = new DatabaseConnector();
+		this.setAllSongs(this.dbconn.getDbSongs());
+		dbconn.printSongs(this.allSongs);
+		for(Song s: this.allSongs){
+			this.currentUser.getAllSongs().add(s);
+		}
+	}	
+	public ArrayList<Song> getAllSongs() {
+		return allSongs;
+	}
+
+	public void setAllSongs(ArrayList<Song> allSongs) {
+		this.allSongs = allSongs;
+	}
+	
+	public void addUser(User user){
+		this.currentUser = user;
+		this.allUsers.add(user);
+	}
+	
+	public User getCurrentUser() {
+		return currentUser;
+	}
+	public void setCurrentUser(User currentUser) {
+		this.currentUser = currentUser;
+	}
+	public Song getCurrentSong() {
+		return currentSong;
+	}
+	public void setCurrentSong(Song currentSong) {
+		this.currentSong = currentSong;
+	}
+	public Boolean getPlaying() {
+		return playing;
+	}
+	public void setPlaying(Boolean playing) {
+		this.playing = playing;
+	}
+
+	public Playlist getCurrentPlaylist() {
+		return currentPlaylist;
+	}
+	public void setCurrentPlaylist(Playlist currentPlaylist) {
+		this.currentPlaylist = currentPlaylist;
+	}
+	public ArrayList<User> getAllUsers() {
+		return allUsers;
+	}
+	public void setAllUsers(ArrayList<User> allUsers) {
+		this.allUsers = allUsers;
+	}
+	public void setMedia(Media media){
+		this.mplayer = new MediaPlayer(media);
+
+	}
+}
